915bb8034ef140ba5b6ac1524e6e5cadc41046cf
Jordan Kay <jordan@fleuronic.com>
2023-05-18 19:02:46 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
 		)
     ],
     dependencies: [
-		.package(url: "https://github.com/Fleuronic/ErgoUIKit", branch: "main"),
+		.package(url: "https://github.com/Fleuronic/Ergo", branch: "main"),
+		.package(url: "https://github.com/Fleuronic/Inject", branch: "main"),
 		.package(url: "https://github.com/Fleuronic/Geometric", branch: "main"),
 		.package(url: "https://github.com/Fleuronic/Telemetric", branch: "main"),
         .package(url: "https://github.com/DeclarativeHub/Layoutless", .upToNextMajor(from: "0.4.0"))
@@ -22,7 +23,8 @@ let package = Package(
 		.target(
 			name: "ErgoDeclarativeUIKit",
 			dependencies: [
-				"ErgoUIKit",
+				"Ergo",
+				"Inject",
 				"Geometric",
 				"Telemetric",
 				"Layoutless"
